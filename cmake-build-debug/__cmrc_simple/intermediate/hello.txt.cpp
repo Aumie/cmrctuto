f30bc741ea71c32365bcbfc78b8a34e81e19be29
Aumie <Aum5376@gmail.com>
2020-05-30 18:15:29 +0700

    first

--- cmake-build-debug/__cmrc_simple/intermediate/hello.txt.cpp
+++
@@ -1,6 +0,0 @@
-        namespace { const char file_array[] = { '\x48','\x65','\x6c','\x6c','\x6f','\x2c','\x20','\x77','\x6f','\x72','\x6c','\x64','\x21', 0 }; }
-        namespace cmrc { namespace simple { namespace res_chars {
-        extern const char* const f_2e54_hello_txt_begin = file_array;
-        extern const char* const f_2e54_hello_txt_end = file_array + 13;
-        }}}
-    
